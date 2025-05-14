c638463da6bac1efbe7b14e05eaf68c60e9b6a5d
sarathchandray <sarathchandray@gmail.com>
2025-05-14 13:14:06 +0300

    controller pagckage created

--- src/main/java/com/visraj/javafeature/controller/FeatureController.java
+++ src/main/java/com/visraj/javafeature/controller/FeatureController.java
@@ -1,8 +1,11 @@
+package com.visraj.javafeature.controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class FeatureController {
+@RequestMapping("/api")
+public class FeatureController {
 
     @GetMapping("/greet")
     public String greet() {
