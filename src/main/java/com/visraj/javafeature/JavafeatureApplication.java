5f37e3cf664bcfc45a7e8030ff6d3905b17f4a5c
sarathchandray <sarathchandray@gmail.com>
2025-05-14 11:05:17 +0300

    Initial commit

---
+++ src/main/java/com/visraj/javafeature/JavafeatureApplication.java
@@ -0,0 +1,13 @@
+package com.visraj.javafeature;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavafeatureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavafeatureApplication.class, args);
+	}
+
+}
